afc2daeba62d0b9e754be1ab70b76c71ed7159c1
jon-mind <113052664+jon-mind@users.noreply.github.com>
2023-04-18 11:11:30 +0000

    added placeholder image

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image||$`{frontend/public/placeholder.png}`}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
